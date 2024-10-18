93aeea4d0a0dabd47b76cf5fedf71785a4c0433a
deepakpant <deepak.93p@gmail.com>
2024-10-18 17:17:01 +0530

    Added async calling in client

--- client/src/proxies/askdocs_proxy.py
+++ client/src/proxies/askdocs_proxy.py
@@ -5,7 +5,7 @@
 import base64
 from typing import Dict
 
-import requests
+from aiohttp import ClientSession
 
 from ..core.config import settings
 from ..core.context import request_id_context
@@ -24,30 +24,38 @@ class AskDocsAPIProxy:
             "X-Request-ID": request_id_context.get(None)
         }
 
-    def _create_auth_header(self, username: str, password: str) -> str:
+    @staticmethod
+    def _create_auth_header(username: str, password: str) -> str:
         credentials = f"{username}:{password}"
         encoded_credentials = base64.b64encode(credentials.encode()).decode()
         return f"Basic {encoded_credentials}"
 
     async def ask(self, request: str) -> AnswerResponse:
         try:
-            response = requests.post(
-                url=f"{self.base_url}/docs/ask",
-                headers=self.headers,
-                json={'question': request}, verify=False)
-
-            logger.info(f"Got answer from ask-docs backend service")
-            logger.debug(f"Response: {response.json()}")
-            return AnswerResponse(**response.json())
+            async with ClientSession() as session:
+                async with session.post(
+                        url=f"{self.base_url}/docs/ask",
+                        headers=self.headers,
+                        json={'question': request},
+                        ssl=False
+                ) as response:
+                    logger.info(f"Got answer from ask-docs backend service")
+                    json_response = await response.json()
+                    logger.debug(f"Response: {json_response}")
+                    return AnswerResponse(**json_response)
         except Exception as e:
             logger.error(f"Failed to get answer from ask-docs backend service: {e}")
             raise
 
     async def health_check(self) -> Dict[str, str]:
         try:
-            response = requests.get(self.health_check_url, verify=False)
-            logger.info(f"Health check passed for ask-docs backend service")
-            return response.json()
+            async with ClientSession() as session:
+                async with session.get(
+                        url=self.health_check_url,
+                        ssl=False
+                ) as response:
+                    logger.info(f"Health check passed for ask-docs backend service")
+                    return response.json()
         except Exception as e:
             logger.error(f"Failed to check health for ask-docs backend service: {e}")
             return {"status": "unhealthy"}
